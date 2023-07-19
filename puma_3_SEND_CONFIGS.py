92990b51e66dcdcdc1d400450c83cf502856b927
Puma_1969 <67933513+elpuma1969@users.noreply.github.com>
2023-07-18 20:21:27 -0400

    Create puma_3_SEND_CONFIGS.py

---
+++ puma_3_SEND_CONFIGS.py
@@ -0,0 +1,66 @@
+import sys
+import os
+import re
+from datetime import datetime
+from netmiko import ConnectHandler
+
+application_path = os.path.dirname(os.path.abspath(sys.argv[0]))
+
+
+def run_script():
+    username = 'puma'
+    password = 'cisco'
+
+    bk_file_path = os.path.join(application_path, 'boxes.txt')
+    with open(bk_file_path) as f:
+        devices_list = f.read().splitlines()
+
+    for device_ip in devices_list:
+        try:
+            print('Connecting to device: ' + device_ip)
+            ios_device = {
+                'device_type': 'cisco_ios',
+                'ip': device_ip,
+                'username': username,
+                'password': password,
+                'secret': password,
+                'port': 22,
+            }
+            connection = ConnectHandler(**ios_device)
+            print('Entering enable mode...')
+
+            # Send the enable command to elevate privilege level
+            connection.enable()
+
+            # Send the desired privileged-level commands
+            config_commands = ['int range fa1/10 - 15', 'no shut', 'switchport mode trunk',
+                               'switchport trunk allowed vlan all', 'end', 'wr']
+            output = connection.send_config_set(config_commands)
+            print(output)
+
+            prompt = connection.find_prompt()
+            hostname = re.search(r'(\S+)[>#]', prompt).group(1)
+
+            now = datetime.now()
+            year = now.year
+            month = now.month
+            day = now.day
+
+            output_directory = os.path.join(application_path, 'BACKUP_SCH', 'Trunk')
+            os.makedirs(output_directory, exist_ok=True)
+            filename = os.path.join(output_directory, f'{hostname}_{year}-{month}-{day}_Down.txt')
+
+            with open(filename, 'w') as final:
+                final.write(output)
+                print(f'Backup of {hostname} completed successfully')
+                print('#' * 30)
+
+            print('Closing connection')
+            connection.disconnect()
+
+        except Exception as e:
+            print(f'An error occurred while processing device {device_ip}: {str(e)}')
+            print('#' * 30)
+
+
+run_script()
